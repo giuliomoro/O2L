2bad4ed9b8d8c3485b1f19e1c80ba6c0f1f37f13
Giulio Moro <giuliomoro@yahoo.it>
2024-10-22 21:59:40 +0000

    Pass extra run.sh commands to make

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-make -C /root/Bela PROJECT_TYPE=cpp SHOULD_BUILD=true HAS_RUN_FILE=false PROJECT=O2L AT=
+make -C /root/Bela PROJECT_TYPE=cpp SHOULD_BUILD=true HAS_RUN_FILE=false PROJECT=O2L AT= $@
 cp O2L.service /lib/systemd/system/
 systemctl enable O2L && systemctl start O2L
