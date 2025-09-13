f1a5a294dd5bffd72ab6e1e8d008163de8ee0fde
Giulio Moro <giuliomoro@yahoo.it>
2025-09-13 22:46:38 +0000

    systemctl restart

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
+set -e
+
 make -C /root/Bela RUN_FILE=aaaaaaaaaaaaaaaaaaaaaa PROJECT=O2L AT= $@
 cp O2L.service /lib/systemd/system/
-systemctl enable O2L && systemctl start O2L
+systemctl enable O2L && systemctl restart O2L
